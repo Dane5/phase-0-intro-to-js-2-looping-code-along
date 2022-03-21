9ea22107cbd17a7a4594f7ca169df10bc4fbf382
Paschal Ramdan <dane.ramdan@gmail.com>
2022-03-21 12:29:46 -0400

    commit tree 326391c7d40e23cc1dd7da4fac9174e29f60f76d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(name, event){
+    let thankYouCard = []  
+     for (let i = 0; i <name.length; i++) {
+         thankYouCard.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  
+    }
+    return thankYouCard;
+ }
+ function countDown(count){
+ while (count >0) {
+     console.log(count)
+ count-=1;
+ }
+ console.log(count)
+                 
+                    }
